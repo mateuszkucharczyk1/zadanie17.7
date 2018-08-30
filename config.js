a2e91a0408dd8d53fcbfdb42be4b3a7dc4f4426f
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-08-30 17:42:48 +0200

    first upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '75042757325-dltq32qt37otgmlmusosf3j39tgu4al8.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'elQNvL6DiBHR7g8gdIyrOFNx',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
